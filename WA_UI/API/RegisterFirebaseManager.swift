4b5223bfe518ff5faabcb783e82d8a2a667a46fb
balaji2417 <48558089+balaji2417@users.noreply.github.com>
2025-12-02 11:50:38 -0500

    Back end routes done for register with validation

--- WA_UI/API/RegisterFirebaseManager.swift
+++ WA_UI/API/RegisterFirebaseManager.swift
@@ -1,112 +1,139 @@
-////
-////  RegisterFirebaseManager.swift
-////  WA_UI
-////
-////  Created by Gokhula Krishnan Thangavel on 11/30/25.
-////
 //
-//import Foundation
-//import FirebaseAuth
-//import FirebaseFirestore
-//import FirebaseStorage
+//  RegisterFirebaseManager.swift
+//  WA_UI
 //
-//extension RegisterViewController {
-//    
-//    func uploadProfilePhotoToStorage() {
-//        var profilePhotoURL: URL?
-//        
-//        if let image = pickedImage {
-//            if let jpegData = image.jpegData(compressionQuality: 0.8) {
-//                let storageRef = storage.reference()
-//                let imagesRepo = storageRef.child("imagesUsers")
-//                let imageRef = imagesRepo.child("\(UUID().uuidString).jpg")
-//                
-//                let uploadTask = imageRef.putData(jpegData, completion: { (metadata, error) in
-//                    if error == nil {
-//                        imageRef.downloadURL(completion: { (url, error) in
-//                            if error == nil {
-//                                profilePhotoURL = url
-//                                self.registerUser(photoURL: profilePhotoURL)
-//                            } else {
-//                                print("Error getting download URL: \(String(describing: error))")
-//                                self.registerUser(photoURL: nil)
-//                            }
-//                        })
-//                    } else {
-//                        print("Error uploading image: \(String(describing: error))")
-//                        // Still register even if photo upload fails
-//                        self.registerUser(photoURL: nil)
-//                    }
-//                })
-//            }
-//        } else {
-//            registerUser(photoURL: nil)
-//        }
-//    }
-//    
-//    func registerUser(photoURL: URL?) {
-//        if let name = registerScreen.nameTextField.text,
-//           let email = registerScreen.emailTextField.text,
-//           let password = registerScreen.passwordTextField.text {
-//            
-//            Auth.auth().createUser(withEmail: email, password: password, completion: { result, error in
-//                if error == nil {
-//                    
-//                    guard let newUserID = result?.user.uid else {
-//                        print("Error: User was created but we could not get their UID.")
-//                        return
-//                    }
-//                    
-//                    // Pass name, email, uid, AND photoURL to save function
-//                    self.saveUserToFirestore(name: name, email: email, uid: newUserID, photoURL: photoURL)
-//                    
-//                } else {
-//                    print("Error creating user: \(String(describing: error))")
-//                    if let error = error {
-//                        let nsError = error as NSError
-//                        if let authError = AuthErrorCode(rawValue: nsError.code) {
-//                            print("Auth Error Code: \(authError)")
-//                        }
-//                    }
-//                }
-//            })
-//        }
-//    }
+//  Created by Gokhula Krishnan Thangavel on 11/30/25.
 //
-//    func saveUserToFirestore(name: String, email: String, uid: String, photoURL: URL?) {
-//        
-//        // 1. Create the Auth Profile update request
-//        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
-//        changeRequest?.displayName = name
-//        changeRequest?.photoURL = photoURL
-//        
-//        changeRequest?.commitChanges(completion: { (error) in
-//            if let error = error {
-//                print("Error updating Auth profile: \(String(describing: error))")
-//            }
-//            
-//            // 2. Create user data for Firestore
-//            let userData: [String: Any] = [
-//                "name": name,
-//                "email": email,
-//                "avatarURL": photoURL?.absoluteString ?? ""
-//            ]
-//            
-//            // 3. Save the document to Firestore using the UID
-//            let userDocument = Firestore.firestore().collection("users")
-//                                .document(uid)
-//            
-//            userDocument.setData(userData) { (error) in
-//                if let error = error {
-//                    print("Error writing document: \(error)")
-//                } else {
-//                    print("Document successfully written!")
-//                    
-//                    // Success! Go back to the login screen.
-//                    self.navigationController?.popViewController(animated: true)
-//                }
-//            }
-//        })
-//    }
-//    
-//}
+
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
+
+extension RegisterViewController {
+    
+    func uploadProfilePhotoToStorage() {
+        var profilePhotoURL: URL?
+        
+        if let image = pickedImage {
+            if let jpegData = image.jpegData(compressionQuality: 0.8) {
+                let storageRef = storage.reference()
+                let imagesRepo = storageRef.child("imagesUsers")
+                let imageRef = imagesRepo.child("\(UUID().uuidString).jpg")
+                
+                let uploadTask = imageRef.putData(jpegData, completion: { (metadata, error) in
+                    if error == nil {
+                        imageRef.downloadURL(completion: { (url, error) in
+                            if error == nil {
+                                profilePhotoURL = url
+                                print(profilePhotoURL)
+                                self.registerUser(photoURL: profilePhotoURL)
+                            } else {
+                                print("Error getting download URL: \(String(describing: error))")
+                                self.registerUser(photoURL: nil)
+                            }
+                        })
+                    } else {
+                        print("Error uploading image: \(String(describing: error))")
+                        // Still register even if photo upload fails
+                        self.registerUser(photoURL: nil)
+                    }
+                })
+            }
+        } else {
+            registerUser(photoURL: nil)
+        }
+    }
+    
+    func registerUser(photoURL: URL?) {
+        if let name = registerScreen.nameTextField.text,
+           let email = registerScreen.emailTextField.text,
+           let password = registerScreen.passwordTextField.text {
+            
+            Auth.auth().createUser(withEmail: email, password: password, completion: { result, error in
+                if error == nil {
+                    
+                    guard let newUserID = result?.user.uid else {
+                        print("Error: User was created but we could not get their UID.")
+                        return
+                    }
+                    
+                    // Pass name, email, uid, AND photoURL to save function
+                    self.saveUserToFirestore(name: name, email: email, uid: newUserID, photoURL: photoURL)
+                    
+                } else {
+                    print(error)
+                    if let error = error {
+                        let nsError = error as NSError
+                        
+                        if let authError = AuthErrorCode(rawValue: nsError.code) {
+                            
+                            print("NS Error :\(authError)")
+                            switch authError {
+                                
+                            case .emailAlreadyInUse:
+                                print("Im here")
+                                
+                                self.showAlert(message: "This email address is already in use by another account.")
+                                
+                            case .weakPassword:
+                                
+                                self.showAlert(message: "The password is too weak. It must be at least 6 characters.")
+                                
+                            case .invalidEmail:
+                                
+                                self.showAlert(message: "The email address is badly formatted.")
+                                
+                            default:
+                                
+                                self.showAlert(message: "An unknown error occurred. Please try again.")
+                            }
+                            
+                        } else {
+                            
+                            self.showAlert(message: "An error occurred: \(error.localizedDescription)")
+                        }
+                        
+                    }
+                }
+            })
+        }
+    }
+
+    func saveUserToFirestore(name: String, email: String, uid: String, photoURL: URL?) {
+        
+        // 1. Create the Auth Profile update request
+        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+        changeRequest?.displayName = name
+        changeRequest?.photoURL = photoURL
+        
+        changeRequest?.commitChanges(completion: { (error) in
+            if let error = error {
+                print("Error updating Auth profile: \(String(describing: error))")
+            }
+            
+            // 2. Create user data for Firestore
+            let userData: [String: Any] = [
+                "name": name,
+                "email": email,
+                "avatarURL": photoURL?.absoluteString ?? ""
+            ]
+            
+            // 3. Save the document to Firestore using the UID
+            let userDocument = Firestore.firestore().collection("users")
+                                .document(uid)
+            
+            userDocument.setData(userData) { (error) in
+                if let error = error {
+                    print("Error writing document: \(error)")
+                } else {
+                    print("Document successfully written!")
+                    
+                    // Success! Go back to the login screen.
+                    self.navigationController?.popViewController(animated: true)
+                }
+            }
+        })
+    }
+    
+}
--- WA_UI/Register/RegisterViewController.swift
+++ WA_UI/Register/RegisterViewController.swift
@@ -17,6 +17,7 @@ class RegisterViewController: UIViewController {
     
     let registerScreen = RegisterView()
     let storage = Storage.storage()
+    var pickedImage:UIImage?
     
     var database: Firestore {
         return Firestore.firestore()
@@ -27,14 +28,79 @@ class RegisterViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerScreen.buttonTakePhoto.menu = getMenuImagePicker()
         var registerButton = registerScreen.registerButton
         registerButton?.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
 
     }
-    
-    @objc func registerButtonTapped() {
-        navigationController?.pushViewController(ViewController(), animated: true)
+    func getMenuImagePicker() -> UIMenu{
+        let menuItems = [
+            UIAction(title: "Camera",handler: {(_) in
+                self.pickUsingCamera()
+            }),
+            UIAction(title: "Gallery",handler: {(_) in
+                self.pickPhotoFromGallery()
+            })
+        ]
+        
+        return UIMenu(title: "Select source", children: menuItems)
     }
     
+    func pickUsingCamera(){
+           let cameraController = UIImagePickerController()
+           cameraController.sourceType = .camera
+           cameraController.allowsEditing = true
+           cameraController.delegate = self
+           present(cameraController, animated: true)
+       }
+       
+       
+       func pickPhotoFromGallery(){
+           //MARK: Photo from Gallery...
+           var configuration = PHPickerConfiguration()
+           configuration.filter = PHPickerFilter.any(of: [.images])
+           configuration.selectionLimit = 1
+           
+           let photoPicker = PHPickerViewController(configuration: configuration)
+           
+           photoPicker.delegate = self
+           present(photoPicker, animated: true, completion: nil)
+       }
+    @objc func registerButtonTapped() {
+        let name = registerScreen.nameTextField.text
+        let password = registerScreen.passwordTextField.text
+        let confirmPassword = registerScreen.confirmPasswordTextField.text
+        
+        if let uwName = name {
+            if(!uwName.isEmpty) {
+                if let uwPassword = password {
+                    if let uwConfirmPassword = confirmPassword {
+                        if(uwPassword == uwConfirmPassword) {
+                            uploadProfilePhotoToStorage()
+                            
+                        }
+                        else {
+                            showAlert(message: "Passwords do not Match..")
+                        }
+                    }
+                }
+            }
+            else {
+                showAlert(message: "Name Cannot Be Empty")
+            }
+        }
+    }
+    
+    func showAlert(message: String) {
+        let alertController = UIAlertController(title: "Error!", message: message, preferredStyle: .alert)
+            
+          
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+           
+            alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
 
 }
+
